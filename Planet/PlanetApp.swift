b1d3a11f8da3fcb4ababebaed9936c04279f66ad
Berkay Özdemir <berkay.ozdemir@optiyol.com>
2024-02-20 16:53:23 +0300

    Initial Commit

---
+++ Planet/PlanetApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanetApp.swift
+//  Planet
+//
+//  Created by Berkay on 20.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct PlanetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
